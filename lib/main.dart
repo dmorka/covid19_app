3a9be4ee2113eb807a277b65d099660ce46192cb
dmorka <morkadawid@gmail.com>
2020-11-03 16:05:22 +0100

    Added class AppColors to consts.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 // Root of the aplication
 
-import 'package:flutter/cupertino.dart';
+import 'package:covid19_app/pages/intro_page.dart';
 import 'package:flutter/material.dart';
 
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'covid19_APP',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-      fontFamily: "Ubuntu",
-    )
-    home: IntroPage(),
-  );
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID 19 App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "Ubuntu",
+      ),
+      home: IntroPage(),
+    );
+  }
 }
