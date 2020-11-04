32fccc3ced3dba69a0635c30194366eb47a71ece
Artur <artur300998@gmail.com>
2020-11-05 00:50:57 +0100

    Added Announcements page and sample items to it. New button on home page to access announcements.

--- lib/core/consts.dart
+++ lib/core/consts.dart
@@ -2,3 +2,30 @@ import 'package:flutter/material.dart';
 
 const backgroundColor = Color(0XFFeffedf2);
 const mainColor = Color(0XFF8d12fe);
+
+const ANNOUNCEMENT_DATA = [
+  {
+    "name":"Anna",
+    "description":"I need help with...",
+    "when":"05-11-2020 11:20",
+    "where":"New York, Queens"
+  },
+  {
+    "name":"George",
+    "description":"I need help with...",
+    "when":"07-11-2020 15:30",
+    "where":"New York, Manhattan"
+  },
+  {
+    "name":"Donald",
+    "description":"Can you help me with...",
+    "when":"09-11-2020 09:10",
+    "where":"New York, Manhattan"
+  },
+  {
+    "name":"Peter",
+    "description":"I am in need of...",
+    "when":"ASAP",
+    "where":"New York, Bronx"
+  }
+];
---
+++ lib/pages/announcements_page.dart
@@ -0,0 +1,96 @@
+
+import 'package:flutter/material.dart';
+import 'package:covid19_app/core/consts.dart';
+import 'package:covid19_app/components/list_item.dart';
+import 'package:covid19_app/components/custom_appbar_widget.dart';
+
+class AnnouncementsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AnnouncementsPageState();
+}
+
+class _AnnouncementsPageState extends State<AnnouncementsPage> {
+  List<Widget> itemsData = [];
+  ScrollController controller = ScrollController();
+
+  void getPostsData() {
+    List<dynamic> responseList = ANNOUNCEMENT_DATA;
+    List<Widget> listItems = [];
+    responseList.forEach((post) {
+      listItems.add(ListItem(post));
+    });
+    setState(() {
+      itemsData = listItems;
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: backgroundColor,
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: 275,
+              decoration: BoxDecoration(
+                color: mainColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+              ),
+              padding: EdgeInsets.only(top: 25, bottom: 30),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Image.asset("assets/images/virus2.png"),
+              ),
+            ),
+            Container(
+              child: ListView(
+                children: <Widget>[
+                  CustomAppBarWidget(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      "ANNOUNCEMENTS",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: _buildAnnouncements(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+    );
+  }
+
+  Widget _buildAnnouncements() {
+    return ListView.builder(
+        shrinkWrap: true,
+        controller: controller,
+        itemCount: itemsData.length,
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return itemsData[index];
+        }
+    );
+  }
+
+}
