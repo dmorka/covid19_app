2c0dd88d129da9e06fd64490e8ab28ecef0791dd
dmorka <morkadawid@gmail.com>
2020-11-04 20:42:07 +0100

    Added handler for opening user profile page after clicking the avatar on the appbar.

--- lib/components/custom_appbar_widget.dart
+++ lib/components/custom_appbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:covid19_app/core/flutter_icons.dart';
+import 'package:covid19_app/pages/user_profile_page.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppBarWidget extends StatelessWidget {
@@ -14,17 +15,25 @@ class CustomAppBarWidget extends StatelessWidget {
           ),
           onPressed: null,
         ),
-        Container(
-          width: 50,
-          height: 50,
-          margin: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.all(
-                Radius.circular(100),
-              ),
-              image: DecorationImage(
-                  image: AssetImage("assets/images/profile.jpg"))),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => UserProfilePage()),
+            );
+          },
+          child: Container(
+            width: 50,
+            height: 50,
+            margin: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(100),
+                ),
+                image: DecorationImage(
+                    image: AssetImage("assets/images/profile.jpg"))),
+          ),
         )
       ],
     );
