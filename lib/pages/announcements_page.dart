c59d36f435b4fd6c180b4e73d4fb66fc54a39880
kacwl <kac.wlodarczyk@gmail.com>
2020-11-05 01:44:39 +0100

    added menu drawer

--- lib/pages/announcements_page.dart
+++ lib/pages/announcements_page.dart
@@ -1,4 +1,4 @@
-
+import 'package:covid19_app/components/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:covid19_app/core/consts.dart';
 import 'package:covid19_app/components/list_item.dart';
@@ -24,60 +24,59 @@ class _AnnouncementsPageState extends State<AnnouncementsPage> {
     });
   }
 
-
   @override
   void initState() {
     super.initState();
     getPostsData();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: backgroundColor,
-        body: Stack(
-          children: <Widget>[
-            Container(
-              height: 275,
-              decoration: BoxDecoration(
-                color: mainColor,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(25),
-                  bottomRight: Radius.circular(25),
-                ),
-              ),
-              padding: EdgeInsets.only(top: 25, bottom: 30),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Image.asset("assets/images/virus2.png"),
+      backgroundColor: backgroundColor,
+      drawer: MenuDrawer(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 275,
+            decoration: BoxDecoration(
+              color: mainColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
               ),
             ),
-            Container(
-              child: ListView(
-                children: <Widget>[
-                  CustomAppBarWidget(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      "ANNOUNCEMENTS",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32,
-                      ),
+            padding: EdgeInsets.only(top: 25, bottom: 30),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Image.asset("assets/images/virus2.png"),
+            ),
+          ),
+          Container(
+            child: ListView(
+              children: <Widget>[
+                CustomAppBarWidget(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    "ANNOUNCEMENTS",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
                     ),
                   ),
-                  SizedBox(height: 25),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: _buildAnnouncements(),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+                SizedBox(height: 25),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: _buildAnnouncements(),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -89,8 +88,6 @@ class _AnnouncementsPageState extends State<AnnouncementsPage> {
         physics: BouncingScrollPhysics(),
         itemBuilder: (context, index) {
           return itemsData[index];
-        }
-    );
+        });
   }
-
 }
