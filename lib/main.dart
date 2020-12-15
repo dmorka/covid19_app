0eda09421d97722623681c0cd510fe1054c1d02a
dmorka <morkadawid@gmail.com>
2020-12-15 20:35:17 +0100

    initialized FlutterFire.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
+import 'package:covid19_app/pages/home_page.dart';
 import 'package:covid19_app/pages/intro_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'COVID 19 App',
+      builder: (context, child) => MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child),
       debugShowCheckedModeBanner: false,
+      title: "COVID 19 App",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: "Ubuntu",
+        primaryColor: Colors.purple,
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: IntroPage(),
     );
