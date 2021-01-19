3e34b34ef778e403ca9c28264a523b7a19e16f5e
wuodar <kac.wlodarczyk@gmail.com>
2021-01-19 21:49:03 +0100

    add setAvatar method

--- lib/utils/services/storage_service.dart
+++ lib/utils/services/storage_service.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:firebase_core/firebase_core.dart' as firebase_core;
+import 'package:http/http.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 final firebase_storage.FirebaseStorage storage =
     firebase_storage.FirebaseStorage.instance;
@@ -25,4 +28,24 @@ class FirebaseStorageService {
       print("Avatar Error!\n" + e.toString());
     }
   }
+
+  Future<void> setAvatar(String userId) async {
+    String downloadURL = await firebase_storage.FirebaseStorage.instance
+        .ref('avatars/$userId')
+        .getDownloadURL();
+    if (downloadURL == null)
+      downloadURL = await firebase_storage.FirebaseStorage.instance
+          .ref('avatars/default')
+          .getDownloadURL();
+    // return downloadURL;
+    var documentDirectory = await getApplicationDocumentsDirectory();
+    print("////////////////////////");
+    print(documentDirectory.path);
+    var response = await get(downloadURL);
+    File file = new File(
+        join(documentDirectory.path, 'profile.jpg')
+    );
+    file.writeAsBytesSync(response.bodyBytes);
+  }
+
 }
