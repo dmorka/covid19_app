d8121c85f7267827b7772880455a0e5a67526ce9
wuodar <kac.wlodarczyk@gmail.com>
2021-01-23 16:53:39 +0100

    working on annoucement item in list

--- lib/components/announcements_list_item.dart
+++ lib/components/announcements_list_item.dart
@@ -1,7 +1,10 @@
+// import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:covid19_app/pages/announcement_page.dart';
 import 'package:covid19_app/models/annoucement.dart';
 import 'package:covid19_app/models/user.dart';
+import 'package:covid19_app/core/consts.dart';
 
 class AnnouncementsListItem extends StatelessWidget {
   final Annoucement content;
@@ -24,12 +27,12 @@ class AnnouncementsListItem extends StatelessWidget {
         margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(20.0)),
-            color: Colors.white,
+            color: backgroundColor,
             boxShadow: [
               BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0)
             ]),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -37,33 +40,54 @@ class AnnouncementsListItem extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(content.title,
-                        style: const TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black87)),
-                    SizedBox(height: 10),
-                    Text(
-                      content.description.length > 200
-                          ? content.description.substring(0, 200) + "..."
-                          : content.description,
-                      style: const TextStyle(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.normal,
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+                        color: mainColor,
                       ),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                              child: Text(content.title,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87,
+                                  ))),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        // borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+                        color: backgroundColor,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                        child: Text(
+                          content.description.length > 200
+                              ? content.description.substring(0, 200) + "..."
+                              : content.description,
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),),
                     ),
                     SizedBox(height: 35),
                     Text(
                       "Czas dostarczenia: " + content.formatedDate(),
-                      style: const TextStyle(
+                      style: TextStyle(
                         color: Colors.black45,
                         fontWeight: FontWeight.normal,
                       ),
                       textAlign: TextAlign.right,
                     ),
                     Text(
-                      "Gdzie: " + content.address.getFullAddress(),
-                      style: const TextStyle(
+                      "Gdzie: ",
+                      style: TextStyle(
                           color: Colors.black45, fontWeight: FontWeight.normal),
                     )
                   ],
